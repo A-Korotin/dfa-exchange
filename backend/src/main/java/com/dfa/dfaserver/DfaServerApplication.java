834e61c2d4446c6d03073a811ae2a3c8b18a19cc
IannDul <dulyoung4d@gmail.com>
2023-10-27 19:24:13 +0300

    initial commit

---
+++ backend/src/main/java/com/dfa/dfaserver/DfaServerApplication.java
@@ -0,0 +1,13 @@
+package com.dfa.dfaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DfaServerApplication.class, args);
+    }
+
+}
