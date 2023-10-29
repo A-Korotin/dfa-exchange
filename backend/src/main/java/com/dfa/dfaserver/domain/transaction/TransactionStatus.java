ef64d82b2e9966703a4c43c942277ae468024d3e
Alexey Korotin <sx12fx@gmail.com>
2023-10-29 22:24:34 +0300

    feat: add transaction and wallet domain entities

---
+++ backend/src/main/java/com/dfa/dfaserver/domain/transaction/TransactionStatus.java
@@ -0,0 +1,7 @@
+package com.dfa.dfaserver.domain.transaction;
+
+public enum TransactionStatus {
+    OK,
+    PENDING,
+    CANCELLED
+}
---
+++ backend/src/main/java/com/dfa/dfaserver/domain/transaction/TransactionType.java
@@ -0,0 +1,5 @@
+package com.dfa.dfaserver.domain.transaction;
+
+public enum TransactionType {
+    TRANSFER
+}
