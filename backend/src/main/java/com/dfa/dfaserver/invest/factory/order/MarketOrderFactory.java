1c4e5cad7dbf61c99431ae6763c69cc0a96e6895
Alexey Korotin <sx12fx@gmail.com>
2023-11-07 21:24:19 +0300

    fix: add MarketOrderFactory bean status

--- backend/src/main/java/com/dfa/dfaserver/invest/factory/order/MarketOrderFactory.java
+++ backend/src/main/java/com/dfa/dfaserver/invest/factory/order/MarketOrderFactory.java
@@ -6,7 +6,9 @@ import com.dfa.dfaserver.invest.domain.order.OrderType;
 import com.dfa.dfaserver.invest.domain.order.SellMarketOrder;
 import com.dfa.dfaserver.invest.dto.order.MarketOrderDto;
 import org.mapstruct.ObjectFactory;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MarketOrderFactory extends OrderFactory<MarketOrder, MarketOrderDto> {
     {
         supplierMap.put(OrderType.BUY, BuyMarketOrder::new);
