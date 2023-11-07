b3273b30b7fe8fd7ea7a722b3b87b4be310748b8
Alexey Korotin <sx12fx@gmail.com>
2023-11-07 21:25:04 +0300

    refactor: rename dto fields

--- backend/src/main/java/com/dfa/dfaserver/invest/dto/asset/AssetDto.java
+++ backend/src/main/java/com/dfa/dfaserver/invest/dto/asset/AssetDto.java
@@ -2,9 +2,11 @@ package com.dfa.dfaserver.invest.dto.asset;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class AssetDto {
     @NotNull
     @NotBlank
--- backend/src/main/java/com/dfa/dfaserver/invest/dto/order/MarketOrderDto.java
+++ backend/src/main/java/com/dfa/dfaserver/invest/dto/order/MarketOrderDto.java
@@ -2,13 +2,15 @@ package com.dfa.dfaserver.invest.dto.order;
 
 import com.dfa.dfaserver.invest.domain.order.OrderType;
 import com.dfa.dfaserver.invest.dto.asset.AssetDto;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class MarketOrderDto {
     public OrderType type;
-    public String fromAddress;
-    public String toAddress;
+    public String from;
+    public String to;
     public AssetDto buyAsset;
     public AssetDto sellAsset;
 }
