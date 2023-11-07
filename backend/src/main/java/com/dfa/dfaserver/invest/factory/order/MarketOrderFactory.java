069e00cea25d011ab8307ebcda43bdd9a4c78da4
Alexey Korotin <sx12fx@gmail.com>
2023-11-07 16:09:07 +0300

    feat: add market order DTO

---
+++ backend/src/main/java/com/dfa/dfaserver/invest/factory/order/MarketOrderFactory.java
@@ -0,0 +1,22 @@
+package com.dfa.dfaserver.invest.factory.order;
+
+import com.dfa.dfaserver.invest.domain.order.BuyMarketOrder;
+import com.dfa.dfaserver.invest.domain.order.MarketOrder;
+import com.dfa.dfaserver.invest.domain.order.OrderType;
+import com.dfa.dfaserver.invest.domain.order.SellMarketOrder;
+import com.dfa.dfaserver.invest.dto.order.MarketOrderDto;
+import org.mapstruct.ObjectFactory;
+
+public class MarketOrderFactory extends OrderFactory<MarketOrder, MarketOrderDto> {
+    {
+        supplierMap.put(OrderType.BUY, BuyMarketOrder::new);
+        supplierMap.put(OrderType.SELL, SellMarketOrder::new);
+    }
+
+    @Override
+    @ObjectFactory
+    public MarketOrder create(MarketOrderDto dto) {
+        return supplierMap.get(dto.type).get();
+
+    }
+}
