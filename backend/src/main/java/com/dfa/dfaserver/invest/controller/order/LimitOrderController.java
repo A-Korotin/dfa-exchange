3fefdad24f50bb5dd36d514e7567cc343557f8f6
Alexey Korotin <sx12fx@gmail.com>
2023-11-12 19:48:21 +0300

    docs: add documentation Controllers and required authorization header

--- backend/src/main/java/com/dfa/dfaserver/invest/controller/order/LimitOrderController.java
+++ backend/src/main/java/com/dfa/dfaserver/invest/controller/order/LimitOrderController.java
@@ -1,12 +1,16 @@
 package com.dfa.dfaserver.invest.controller.order;
 
 import com.dfa.dfaserver.invest.domain.order.LimitOrder;
+import com.dfa.dfaserver.invest.dto.error.ErrorDto;
 import com.dfa.dfaserver.invest.dto.order.InputLimitOrderDto;
 import com.dfa.dfaserver.invest.dto.order.LimitOrderDto;
 import com.dfa.dfaserver.invest.exception.NotFoundException;
 import com.dfa.dfaserver.invest.mapper.order.LimitOrderMapper;
 import com.dfa.dfaserver.invest.service.order.LimitOrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -29,6 +33,11 @@ public class LimitOrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create limit order",
+            description = "Create limit order and automatically send it to the execution queue",
+            responses = {@ApiResponse(description = "Created", responseCode = "201", useReturnTypeSchema = true),
+                    @ApiResponse(description = "Bad request", responseCode = "400", content = {@Content(schema = @Schema(implementation = ErrorDto.class))}),
+                    @ApiResponse(description = "Not authorized", responseCode = "401", content = {@Content(schema = @Schema(implementation = Void.class))})})
     public LimitOrderDto createLimitOrder(@Valid @RequestBody InputLimitOrderDto dto) {
         LimitOrder order = mapper.fromDto(dto);
         order = limitOrderService.save(order);
@@ -36,6 +45,12 @@ public class LimitOrderController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get limit order",
+            description = "Get limit order by ID",
+            responses = {@ApiResponse(description = "OK", responseCode = "200", useReturnTypeSchema = true),
+                    @ApiResponse(description = "Bad request", responseCode = "400", content = {@Content(schema = @Schema(implementation = ErrorDto.class))}),
+                    @ApiResponse(description = "Not authorized", responseCode = "401", content = {@Content(schema = @Schema(implementation = Void.class))}),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = {@Content(schema = @Schema(implementation = Void.class))})})
     public LimitOrderDto getLimitOrder(@PathVariable UUID id) {
         LimitOrder order = limitOrderService.findById(id)
                 .orElseThrow(() -> new NotFoundException("Limit order with id '%s' could not be found", id));
@@ -44,6 +59,12 @@ public class LimitOrderController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Edit limit order",
+            description = "Edit limit order by ID",
+            responses = {@ApiResponse(description = "OK", responseCode = "200", useReturnTypeSchema = true),
+                    @ApiResponse(description = "Bad request", responseCode = "400", content = {@Content(schema = @Schema(implementation = ErrorDto.class))}),
+                    @ApiResponse(description = "Not authorized", responseCode = "401", content = {@Content(schema = @Schema(implementation = Void.class))}),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = {@Content(schema = @Schema(implementation = Void.class))})})
     public LimitOrderDto editLimitOrder(@PathVariable UUID id,
                                         @Valid @RequestBody InputLimitOrderDto dto) {
         LimitOrder order = mapper.fromDto(dto);
@@ -53,11 +74,21 @@ public class LimitOrderController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete limit order",
+            description = "Delete limit order by ID",
+            responses = {@ApiResponse(description = "OK", responseCode = "200", useReturnTypeSchema = true),
+                    @ApiResponse(description = "Bad request", responseCode = "400", content = {@Content(schema = @Schema(implementation = ErrorDto.class))}),
+                    @ApiResponse(description = "Not authorized", responseCode = "401", content = {@Content(schema = @Schema(implementation = Void.class))}),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = {@Content(schema = @Schema(implementation = Void.class))})})
     public void deleteLimitOrder(@PathVariable UUID id) {
         limitOrderService.deleteById(id);
     }
 
     @GetMapping
+    @Operation(summary = "Get all limit orders",
+            description = "Get all limit orders",
+            responses = {@ApiResponse(description = "OK", responseCode = "200", useReturnTypeSchema = true),
+                    @ApiResponse(description = "Not authorized", responseCode = "401", content = {@Content(schema = @Schema(implementation = Void.class))})})
     public List<LimitOrderDto> getAllLimitOrders() {
         return limitOrderService.findAll().stream()
                 .map(mapper::toDto)
