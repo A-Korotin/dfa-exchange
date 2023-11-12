ce098d5beeb3614fabf7ee3a1e3e2f46dc771310
Alexey Korotin <sx12fx@gmail.com>
2023-11-12 19:17:53 +0300

    feat: add ErrorDto

--- backend/src/main/java/com/dfa/dfaserver/invest/controller/order/advice/OrderControllerAdvice.java
+++ backend/src/main/java/com/dfa/dfaserver/invest/controller/order/advice/OrderControllerAdvice.java
@@ -1,23 +1,20 @@
 package com.dfa.dfaserver.invest.controller.order.advice;
 
+import com.dfa.dfaserver.invest.dto.error.ErrorDto;
 import com.dfa.dfaserver.invest.exception.BaseException;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Collections;
-import java.util.Map;
-
 @RestControllerAdvice
 @Slf4j
 public class OrderControllerAdvice {
 
     @ExceptionHandler(BaseException.class)
-    public Map<String, String> handleBase(BaseException e, HttpServletResponse response) {
+    public ErrorDto handleBase(BaseException e, HttpServletResponse response) {
         response.setStatus(e.getStatus().value());
         log.warn("{} occurred with cause: {}", e.getClass().getSimpleName(), e.getMessage());
-        // TODO: 10.11.2023 create error dto 
-        return Collections.singletonMap("message", e.getMessage());
+        return new ErrorDto(e.getMessage());
     }
 }
---
+++ backend/src/main/java/com/dfa/dfaserver/invest/dto/error/ErrorDto.java
@@ -0,0 +1,12 @@
+package com.dfa.dfaserver.invest.dto.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDto {
+    public String message;
+}
