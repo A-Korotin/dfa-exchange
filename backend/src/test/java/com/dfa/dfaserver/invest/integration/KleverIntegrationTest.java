180a8b7c2347d4cbafe49a27e938dde086077b94
Alexey Korotin <sx12fx@gmail.com>
2023-11-23 12:23:38 +0300

    test: enhance KleverIntegrationTest scenario

--- backend/src/test/java/com/dfa/dfaserver/invest/integration/KleverIntegrationTest.java
+++ backend/src/test/java/com/dfa/dfaserver/invest/integration/KleverIntegrationTest.java
@@ -1,12 +1,15 @@
 package com.dfa.dfaserver.invest.integration;
 
 import com.dfa.dfaserver.invest.service.integration.impl.klever.KleverOISProvider;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
@@ -18,8 +21,9 @@ public class KleverIntegrationTest {
 
 
     @Test
+    @DisplayName("Test Klever API asset fetching")
     public void testAssetHttpRequest() {
         var assets = oisProvider.getAssets().join();
-        System.out.println(assets);
+        assertTrue(assets.size() > 0, "Asset list should be non-empty");
     }
 }
