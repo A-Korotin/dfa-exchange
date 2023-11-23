41890f82d2ee96e6738cbb478f64820c52e87dc4
Alexey Korotin <sx12fx@gmail.com>
2023-11-23 12:22:31 +0300

    refactor: add custom exception, minor refactoring

---
+++ backend/src/main/java/com/dfa/dfaserver/invest/exception/integration/InvalidBlockchainResponseException.java
@@ -0,0 +1,8 @@
+package com.dfa.dfaserver.invest.exception.integration;
+
+public class InvalidBlockchainResponseException extends RuntimeException {
+
+    public InvalidBlockchainResponseException(String message) {
+        super(message);
+    }
+}
--- backend/src/main/java/com/dfa/dfaserver/invest/service/integration/impl/klever/KleverOISProvider.java
+++ backend/src/main/java/com/dfa/dfaserver/invest/service/integration/impl/klever/KleverOISProvider.java
@@ -4,14 +4,13 @@ import com.dfa.dfaserver.invest.domain.Account;
 import com.dfa.dfaserver.invest.domain.Asset;
 import com.dfa.dfaserver.invest.domain.OISChain;
 import com.dfa.dfaserver.invest.domain.transaction.Transaction;
+import com.dfa.dfaserver.invest.exception.integration.InvalidBlockchainResponseException;
 import com.dfa.dfaserver.invest.service.integration.Balance;
 import com.dfa.dfaserver.invest.service.integration.OISProvider;
 import com.dfa.dfaserver.invest.service.integration.impl.klever.dto.KleverAssetDto;
 import com.dfa.dfaserver.invest.service.integration.impl.klever.dto.KleverAssetsPageDto;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -42,31 +41,49 @@ public class KleverOISProvider implements OISProvider {
     private final OISChain kleverChain = new OISChain("Klever");
 
 
+    private KleverAssetsPageDto getPage(JsonNode root) throws JsonProcessingException {
+        JsonNode paginationJson = root.get("pagination");
+        return mapper.treeToValue(paginationJson, KleverAssetsPageDto.class);
+    }
+
+    private List<KleverAssetDto> getAssets(JsonNode root) throws JsonProcessingException {
+        JavaType kleverAssetsType = mapper.getTypeFactory()
+                .constructCollectionType(List.class, KleverAssetDto.class);
+
+
+        JsonNode assetsJson = root.path("data").path("assets");
+        return mapper.treeToValue(assetsJson, kleverAssetsType);
+
+    }
+
     private List<Asset> kleverAssetsHttpRequest() {
         List<Asset> assets = new ArrayList<>();
 
-        JavaType kleverAssetsType = mapper.getTypeFactory()
-                .constructCollectionType(List.class, KleverAssetDto.class);
         KleverAssetsPageDto page = new KleverAssetsPageDto();
         page.next = 1;
         try {
             do {
-                ResponseEntity<String> response = template.getForEntity(assetsUrl + "?page=" + page.next, String.class);
+
+                ResponseEntity<String> response = template
+                        .getForEntity(assetsUrl + "?limit=100&page=" + page.next, String.class);
+
                 if (response.getStatusCode() != HttpStatusCode.valueOf(200)) {
-                    throw new RuntimeException("Klever api returned non-ok status code '%s'".formatted(response.getStatusCode()));
+                    throw new InvalidBlockchainResponseException("Klever api returned non-ok status code '%s'"
+                            .formatted(response.getStatusCode()));
                 }
 
                 JsonNode root = mapper.readTree(response.getBody());
-                JsonNode paginationJson = root.get("pagination");
-                page = mapper.treeToValue(paginationJson, KleverAssetsPageDto.class);
-                JsonNode assetsJson = root.path("data").path("assets");
-                List<KleverAssetDto> assetsRetrieved = mapper.treeToValue(assetsJson, kleverAssetsType);
+                page = getPage(root);
+                List<KleverAssetDto> assetsRetrieved = getAssets(root);
+
                 assetsRetrieved.stream().map(kleverAsset -> new Asset(kleverAsset.getTicker(), kleverChain))
                         .forEach(assets::add);
+
             } while (page.self != page.totalPages);
         } catch (JsonProcessingException exception) {
-            throw new RuntimeException(exception.getMessage());
+            throw new InvalidBlockchainResponseException(exception.getMessage());
         }
+
         return assets;
     }
 
